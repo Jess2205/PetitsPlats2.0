93b4621e4f794fce9b559808300cfb992d9fc216
Isidore <jessica.muschke79@gmail.com>
2024-10-04 23:37:14 +0200

    Efface message d'erreur avec la croix

--- script/main.js
+++ script/main.js
@@ -97,6 +97,7 @@ document.getElementById('search-input').addEventListener('input', function () {
   // Vérifiez si le champ de recherche a du texte
   if (this.value.length > 0) {
     clearBtn.classList.remove('hidden'); // Affiche la croix si du texte est saisi
+    hideErrorMessage();
   } else {
     clearBtn.classList.add('hidden'); // Masque la croix si l'input est vide
   }
@@ -107,6 +108,7 @@ document.getElementById('main-clear-search').addEventListener('click', function 
   const searchInput = document.getElementById('search-input');
   searchInput.value = ''; // Efface le texte de l'input
   this.classList.add('hidden'); // Masque la croix après avoir effacé le texte
+  hideErrorMessage();
   searchInput.focus(); // Remet le focus sur l'input après avoir effacé
 });
 
