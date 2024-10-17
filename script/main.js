7c6f50ebd4bb66553542ddc9aa45c0ddbcd6a50c
Isidore <jessica.muschke79@gmail.com>
2024-10-17 11:52:59 +0200

    // Branche 2 : Boucles While et For

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ import { displayRecipes, showErrorMessage, hideErrorMessage } from './index.js';
 import { selectedTags, filterOptions, updateAdvancedFilters } from './filtres.js';// Importation de la fonction qui affiche les tags
 import { updateRecipeCount} from './index.js';
 
-
+// Branche 2 : Boucles While et For
 // Fonction pour masquer les recettes en vidant le conteneur
 function hideRecipes() {
   const recipeContainer = document.getElementById('results-container');
